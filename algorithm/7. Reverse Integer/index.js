979f025ff056701d3458d99bc41a29776d01adc9
Koki Akamatsu <list6311@gmail.com>
2020-10-31 23:51:01 +0900

    add fikes

---
+++ algorithm/7. Reverse Integer/index.js
@@ -0,0 +1,10 @@
+var reverse = function(x) {
+    if (x >= 0){
+        const result = String(x);
+        return result;
+    } else {
+        const result = String(x);
+        return result;
+    }
+};
+console.log(reverse(-123));
