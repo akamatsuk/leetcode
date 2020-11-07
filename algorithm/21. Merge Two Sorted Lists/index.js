462c09e64611eaba5ed10b5a4eba0ad614dbf680
Koki Akamatsu <list6311@gmail.com>
2020-11-07 23:39:41 +0900

    sort dele

--- algorithm/21. Merge Two Sorted Lists/index.js
+++ algorithm/21. Merge Two Sorted Lists/index.js
@@ -10,6 +10,9 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+
+
 var mergeTwoLists = function(l1, l2) {
     let i = 0;
 
@@ -17,7 +20,7 @@ var mergeTwoLists = function(l1, l2) {
         l1.push(l2[i]);
         i++
     }
-    return l1.sort();
+    return l1;
 };
 
 console.log(mergeTwoLists([1,2,4],[1,3,4]));
