b16405044df8b41fb2833abf49e8ccc55025b978
Koki Akamatsu <list6311@gmail.com>
2020-11-10 18:04:56 +0900

    remake

--- algorithm/yss_algo/index.js
+++ algorithm/yss_algo/index.js
@@ -1,15 +1,28 @@
 
+// var shuffle = function(num) {
+//     const arr = String(num).split('');
+//     let aLn = arr.length - 4;
+
+//     for (let i = arr.length -1; i > aLn; i--) {
+//         const j = Math.floor(Math.random() * (i - aLn + 1) + aLn);
+//         [arr[j], arr[i]] = [arr[i], arr[j]]
+//     }
+//     while (arr.length > 4) {
+//         arr.shift();
+//     }
+//     return arr.join('');
+// };
+
 var shuffle = function(num) {
     const arr = String(num).split('');
-    let aLn = arr.length - 4;
-
-    for (let i = arr.length -1; i > aLn; i--) {
-        const j = Math.floor(Math.random() * (i - aLn + 1) + aLn);
-        [arr[j], arr[i]] = [arr[i], arr[j]]
-    }
     while (arr.length > 4) {
         arr.shift();
     }
+    for (let i = arr.length -1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[j], arr[i]] = [arr[i], arr[j]]
+    }
+
     return arr.join('');
 };
 
