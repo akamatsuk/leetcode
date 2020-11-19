6a676a38a208f8f3906790104d477ccc7ee4f5ec
Koki Akamatsu <list6311@gmail.com>
2020-11-19 23:08:47 +0900

    naosi

--- algorithm/21. Merge Two Sorted Lists/index.js
+++ algorithm/21. Merge Two Sorted Lists/index.js
@@ -14,13 +14,8 @@
 
 
 var mergeTwoLists = function(l1, l2) {
-    let i = 0;
-
-    while (i<l2.length){
-        l1.push(l2[i]);
-        i++
-    }
-    return l1;
+    
+    
 };
 
 console.log(mergeTwoLists([1,2,4],[1,3,4]));
